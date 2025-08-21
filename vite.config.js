46e1f294f4fe821564a449356a0e95938bf0cee6
laibalaraib <laibalaraib2001@gmail.com>
2025-08-21 21:28:21 +0500

    fix: update Vite base path and config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/LL.Tech-Portfolio",
+  base:"/LL.Tech-Portfolio/",
 })
